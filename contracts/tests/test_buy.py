d181ee1c8257c3dbcd67f98937c6742ec9273111
Benjamin PILIA <bpilia@octo.com>
2020-10-09 10:55:01 +0200

    Refacto existing tests and adds some test on buy function

--- contracts/tests/test_buy.py
+++ contracts/tests/test_buy.py
@@ -31,29 +31,54 @@ class buyTest(TestCase):
         print("projectdir", project_dir)
         cls.nftContract = ContractInterface.create_from(join(project_dir, 'src/land.tz'))
 
+    def get_storage_with_one_land_on_sale(self, token_id=1, owner=alice, price=100):
+        return {"market": {"fundsByOwner": {},
+                           "buyers": {},
+                           "lands": {},
+                           "admin": "tz1ibMpWS6n6MJn73nQHtK5f4ogyYC1z9T9z",
+                           "height": 10, "width": 10,
+                           "to_sell": {token_id: price}},
+                "ledger": {token_id: owner},
+                "operators": {},
+                "metadata": {"token_defs": [{"from_": 1, "to_": 100}],
+                             "last_used_id": 1,
+                             "metadata": {(1, 100): {"token_id": 1,
+                                                     "symbol": "TLD",
+                                                     "name": "TezosLand",
+                                                     "decimals": 0,
+                                                     "extras": {}
+                                                     }
+                                          }}
+                }
+
+    def get_storage_with_no_land_on_sale(self, token_id=1, owner=alice):
+        return {"market": {"fundsByOwner": {},
+                           "buyers": {},
+                           "lands": {},
+                           "admin": "tz1ibMpWS6n6MJn73nQHtK5f4ogyYC1z9T9z",
+                           "height": 10, "width": 10,
+                           "to_sell": {}},
+                "ledger": {token_id: owner},
+                "operators": {},
+                "metadata": {"token_defs": [{"from_": 1, "to_": 100}],
+                             "last_used_id": 1,
+                             "metadata": {(1, 100): {"token_id": 1,
+                                                     "symbol": "TLD",
+                                                     "name": "TezosLand",
+                                                     "decimals": 0,
+                                                     "extras": {}
+                                                     }
+                                          }}
+                }
+
     def test_a_land_on_sale_can_be_bought_if_the_amount_is_matching_the_price_and_the_buyer_is_not_the_seller(self):
         # GIVEN
         amount_sent = Decimal(0.000100).quantize(Decimal("0.0001"))
         price = 100
         token_id = 1
-        storage_with_alice_owning_a_land = {"market": {"fundsByOwner": {},
-                                                       "buyers": {},
-                                                       "lands": {},
-                                                       "admin": "tz1ibMpWS6n6MJn73nQHtK5f4ogyYC1z9T9z",
-                                                       "height": 10, "width": 10,
-                                                       "to_sell": {token_id: price}},
-                                            "ledger": {token_id: alice},
-                                            "operators": {},
-                                            "metadata": {"token_defs": [{"from_": 1, "to_": 100}],
-                                                         "last_used_id": 1,
-                                                         "metadata": {(1, 100): {"token_id": 1,
-                                                                                 "symbol": "TLD",
-                                                                                 "name": "TezosLand",
-                                                                                 "decimals": 0,
-                                                                                 "extras": {}
-                                                                                 }
-                                                                      }}
-                                            }
+        storage_with_alice_owning_a_land = self.get_storage_with_one_land_on_sale(token_id=token_id,
+                                                                                  owner=alice,
+                                                                                  price=price)
         # WHEN
         result = self.nftContract.buyLand(1).with_amount(amount_sent).result(
             storage=storage_with_alice_owning_a_land,
@@ -65,60 +90,74 @@ class buyTest(TestCase):
         self.assertEqual({bob: amount_sent}, result.big_map_diff['market/fundsByOwner'])
         self.assertEqual({token_id: bob}, result.big_map_diff['market/buyers'])
 
+    def test_a_land_on_sale_cannot_be_bought_if_the_amount_is_not_matching_the_price_and_the_buyer_is_not_the_seller(
+            self):
+        with self.assertRaises(MichelsonRuntimeError) as amount_not_matching_price_error:
+            # GIVEN
+            amount_sent = Decimal(0.000100).quantize(Decimal("0.0001"))
+            price = 200
+            token_id = 1
+            storage_with_alice_owning_a_land = self.get_storage_with_one_land_on_sale(token_id=token_id,
+                                                                                      owner=alice,
+                                                                                      price=price)
+            # WHEN
+            result = self.nftContract.buyLand(1).with_amount(amount_sent).result(
+                storage=storage_with_alice_owning_a_land,
+                source=bob
+            )
+
+        # THEN
+        error_message = str(amount_not_matching_price_error.exception.args[0]['with']['string'])
+        self.assertEqual("The amount sent is not equal to the price of the land", error_message)
+
     def test_a_land_on_sale_cannot_be_bought_if_the_amount_is_matching_the_price_and_the_buyer_is_the_seller(self):
-        with self.assertRaises(MichelsonRuntimeError):
+        with self.assertRaises(MichelsonRuntimeError) as buyer_is_owner_error:
+            # GIVEN
             amount_sent = Decimal(0.000100).quantize(Decimal("0.0001"))
             price = 100
             token_id = 1
-            storage_with_alice_owning_a_land = {"market": {"fundsByOwner": {},
-                                                           "buyers": {},
-                                                           "lands": {},
-                                                           "admin": "tz1ibMpWS6n6MJn73nQHtK5f4ogyYC1z9T9z",
-                                                           "height": 10, "width": 10,
-                                                           "to_sell": {token_id: price}},
-                                                "ledger": {token_id: alice},
-                                                "operators": {},
-                                                "metadata": {"token_defs": [{"from_": 1, "to_": 100}],
-                                                             "last_used_id": 1,
-                                                             "metadata": {(1, 100): {"token_id": 1,
-                                                                                     "symbol": "TLD",
-                                                                                     "name": "TezosLand",
-                                                                                     "decimals": 0,
-                                                                                     "extras": {}
-                                                                                     }
-                                                                          }}
-                                                }
-
+            storage_with_alice_owning_a_land = self.get_storage_with_one_land_on_sale(token_id=token_id,
+                                                                                      owner=alice,
+                                                                                      price=price)
+            # WHEN
             self.nftContract.buyLand(1).with_amount(amount_sent).result(
                 storage=storage_with_alice_owning_a_land,
                 source=alice
             )
 
+        # THEN
+        error_message = str(buyer_is_owner_error.exception.args[0]['with']['string'])
+        self.assertEqual("The buyer is already the owner of the land", error_message)
+
+    def test_a_land_not_on_sale_cannot_be_bought(self):
+        with self.assertRaises(MichelsonRuntimeError) as land_not_on_sale_error:
+            # GIVEN
+            amount_sent = Decimal(0.000100).quantize(Decimal("0.0001"))
+            token_id = 1
+            storage_with_alice_owning_a_land = self.get_storage_with_no_land_on_sale(token_id=token_id, owner=alice)
+
+            # WHEN
+            self.nftContract.buyLand(1).with_amount(amount_sent).result(
+                storage=storage_with_alice_owning_a_land,
+                source=bob
+            )
+
+        # THEN
+        error_message = str(land_not_on_sale_error.exception.args[0]['with']['string'])
+        self.assertEqual("Land is not on sale", error_message)
+
     def test_the_owner_of_a_land_can_sell_its_property(self):
+        # GIVEN
         token_id_sold_by_alice = 1
         alice_land_price = Decimal(0.0003).quantize(Decimal("0.0003"))
-        storage_with_alice_owning_a_land = {"market": {"fundsByOwner": {},
-                                                       "buyers": {},
-                                                       "lands": {},
-                                                       "admin": "tz1ibMpWS6n6MJn73nQHtK5f4ogyYC1z9T9z",
-                                                       "height": 10, "width": 10,
-                                                       "to_sell": {}},
-                                            "ledger": {token_id_sold_by_alice: alice},
-                                            "operators": {(alice, bob, token_id_sold_by_alice): None},
-                                            "metadata": {"token_defs": [{"from_": 1, "to_": 100}],
-                                                         "last_used_id": 1,
-                                                         "metadata": {(1, 100): {"token_id": token_id_sold_by_alice,
-                                                                                 "symbol": "TLD",
-                                                                                 "name": "TezosLand",
-                                                                                 "decimals": 0,
-                                                                                 "extras": {}
-                                                                                 }
-                                                                      }}
-                                            }
+        storage_with_alice_owning_a_land = self.get_storage_with_no_land_on_sale(token_id=token_id_sold_by_alice, owner=alice)
+
+        # WHEN
         result = self.nftContract.sellLand({"id": token_id_sold_by_alice, "price": alice_land_price}).result(
             storage=storage_with_alice_owning_a_land,
             source=alice
         )
+        # THEN
         self.assertEqual({token_id_sold_by_alice: alice_land_price}, result.big_map_diff["market/to_sell"])
 
 
