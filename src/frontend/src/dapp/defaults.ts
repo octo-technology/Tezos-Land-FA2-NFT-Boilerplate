c5e251301fdc7f287a45c237358a34e69d9543b1
benjamin.pilia@octo.com <benjamin.pilia@octo.com>
2021-03-25 19:43:00 +0100

    Adds edonet support

--- src/frontend/src/dapp/defaults.ts
+++ src/frontend/src/dapp/defaults.ts
@@ -1,5 +1,5 @@
 // const { pkh } = require('../../../contracts/build/contracts/TezosLand.json');
 
 export const APP_NAME = 'TEZOSLAND';
-export const NETWORK = 'delphinet';
-export const TEZOSLAND_ADDRESS = 'KT1S5hgipNSTFehZo7v81gq6fcLChbRwptqy';
+export const NETWORK = 'edo2net';
+export const TEZOSLAND_ADDRESS = 'KT1GuwfE5nrLzKizwnqbP5or2jKqPAGC8EfJ';
